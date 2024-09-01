c829b16839c86d2b62c233b34d6b5fe0f40fa7a3
Fatatouille <franciscomiguelgimenez708@gmail.com>
2024-08-31 23:29:17 -0300

    Cadeteria completa. Falta solo Program

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,4 +1,5 @@
 using Cadetes;
+using Pedido;
 
 namespace Cadeteria
 {
@@ -7,5 +8,44 @@ namespace Cadeteria
         private string Nombre;
         private int Telefono;
         private List<Cadete> ListadoCadetes;
+        Random random = new Random();
+
+        public string nombre {get => Nombre;}
+        public int telefono {get=>Telefono;}
+        public List<Cadete> listadoCadetes{get=> ListadoCadetes;}
+
+        public Cadeteria(string nombre, int telefono, List<Cadete> cadetes)
+        {
+            this.Nombre = nombre;
+            this.Telefono = telefono;
+            this.ListadoCadetes = cadetes;
+        }
+
+        public void AsignarPedido(Pedidos pedido){
+            if(ListadoCadetes.Count == 0)
+            {
+                Console.WriteLine("No hay cadetes disponibles.");
+            }else{
+                Cadete cadete = ListadoCadetes[random.Next(ListadoCadetes.Count)];
+                cadete.AgregarPedido(pedido);
+
+                Console.WriteLine($"El cadete {cadete.nombre} está a cargo del pedido {pedido.Nro}");
+            }
+        }
+        public void ReasignarCadete(Pedidos pedido, Cadete cadete)
+        {
+            cadete.EliminarPedido(pedido, 1);
+            Cadete NuevoCadete = ListadoCadetes[random.Next(ListadoCadetes.Count)];
+            NuevoCadete.AgregarPedido(pedido);
+            Console.WriteLine($"El pedido Nro {pedido.Nro} fue reasignado al cadete {NuevoCadete.nombre}");
+        }
+        public void AgregarCadete(Cadete cadete)
+        {
+            ListadoCadetes.Add(cadete);
+        }
+        public void DespedirCadete(Cadete cadete)
+        {
+            ListadoCadetes.Remove(cadete);
+        }
     }
 }
